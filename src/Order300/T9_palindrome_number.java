c3d59f63f2cd801b90b213c2742533bfa28be7f3
章一砚 <1052886775@qq.com>
2021-04-21 22:20:37 +0800

    改为字符串法，从 150+ms 优化到了 10~12ms

--- src/Order300/T9_palindrome_number.java
+++ src/Order300/T9_palindrome_number.java
@@ -15,13 +15,23 @@ public class T9_palindrome_number {
       if (x == 0) return true;
       if (x < 0 || x % 10 == 0) return false;
 
-      int reverseNum = 0;
-      while (reverseNum < x) {
-        reverseNum = reverseNum * 10 + x % 10;
-        x /= 10;
-        System.out.println(reverseNum + "\t" + x);
+      /* 字符串法 10~12ms 10ms => 73% */
+      char[] line = String.valueOf(x).toCharArray();
+      int len = line.length;
+      for (int i = 0; i < len / 2; i++) {
+        if (line[i] != line[len - 1 - i]) {
+          return false;
+        }
       }
-      return reverseNum == x || reverseNum / 10 == x;
+      return true;
+      /* 数学演算法 150ms+ => 5.51%  */
+      // int reverseNum = 0;
+      // while (reverseNum < x) {
+      //   reverseNum = reverseNum * 10 + x % 10;
+      //   x /= 10;
+      //   System.out.println(reverseNum + "\t" + x);
+      // }
+      // return reverseNum == x || reverseNum / 10 == x;
     }
   }
 }
