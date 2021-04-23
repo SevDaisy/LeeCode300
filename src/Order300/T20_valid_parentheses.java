483b1887c2e560a22d16b467893cf32de1694c92
章一砚 <1052886775@qq.com>
2021-04-23 19:37:43 +0800

    图方便用函数代替了HashSet 2ms => 77.11% OK的啦～

---
+++ src/Order300/T20_valid_parentheses.java
@@ -0,0 +1,45 @@
+package Order300;
+
+import java.util.Stack;
+
+public class T20_valid_parentheses {
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().isValid("()"));
+    System.out.println(new Solution().isValid("()[]{}"));
+    System.out.println(new Solution().isValid("([)]"));
+  }
+
+  static class Solution {
+
+    char pair(char right) {
+      switch (right) {
+        case ')':
+          return '(';
+        case ']':
+          return '[';
+        case '}':
+          return '{';
+        default:
+          return right;
+      }
+    }
+
+    boolean left(char x) {
+      if (x == '(' || x == '[' || x == '{') return true; else return false;
+    }
+
+    public boolean isValid(String s) {
+      int iMax = s.length();
+      Stack<Character> stack = new Stack<>();
+      for (int i = 0; i < iMax; i++) {
+        if (left(s.charAt(i))) {
+          stack.push(s.charAt(i));
+        } else if (
+          stack.isEmpty() || stack.pop() != pair(s.charAt(i))
+        ) return false;
+      }
+      return stack.isEmpty();
+    }
+  }
+}
