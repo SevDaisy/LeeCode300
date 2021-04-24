d9e8c02926dcfc7d8c4a5890f15289bfc0e25884
章一砚 <1052886775@qq.com>
2021-04-24 11:42:04 +0800

    T32 补充说明了为什么官方代码比我的简洁

--- src/Order300/T32_longest_valid_parentheses.java
+++ src/Order300/T32_longest_valid_parentheses.java
@@ -2,14 +2,30 @@ package Order300;
 
 public class T32_longest_valid_parentheses {
 
+  /** 本地调试时用于打印 s 和 dp */
+  static void printDP(String s, int[] dp) {
+    StringBuilder sdp = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      System.out.print(s.charAt(i));
+      System.out.print(" ");
+      sdp.append(String.valueOf(dp[i]));
+      sdp.append(" ");
+    }
+    System.out.println();
+    System.out.println(sdp.toString());
+  }
+
   public static void main(String[] args) {
-    System.out.println(new Solution().longestValidParentheses("()")); // -> 2
-    System.out.println(new Solution().longestValidParentheses("(")); // -> 0
-    System.out.println(new Solution().longestValidParentheses(")")); // -> 0
-    System.out.println(new Solution().longestValidParentheses("(()")); // -> 2
-    System.out.println(new Solution().longestValidParentheses("(())))")); // -> 4
-    System.out.println(new Solution().longestValidParentheses("(())()))")); // -> 6
-    System.out.println(new Solution().longestValidParentheses("(()())()))")); // -> 8
+    // System.out.println(new Solution().longestValidParentheses("()")); // -> 2
+    // System.out.println(new Solution().longestValidParentheses("(")); // -> 0
+    // System.out.println(new Solution().longestValidParentheses(")")); // -> 0
+    // System.out.println(new Solution().longestValidParentheses("(()")); // -> 2
+    // System.out.println(new Solution().longestValidParentheses("(())))")); // -> 4
+    System.out.println(new Solution().longestValidParentheses(")(())()))")); // -> 6
+    System.out.println(
+      new Solution_leecode().longestValidParentheses(")(())()))")
+    ); // -> 6
+    // System.out.println(new Solution().longestValidParentheses("(()())()))")); // -> 8
   }
 
   static class Solution {
@@ -41,10 +57,10 @@ public class T32_longest_valid_parentheses {
           /* 考虑形如 ....((..)) */
           if (i - (preLen + 1) >= 0 && s.charAt(i - (preLen + 1)) == '(') {
             if (i - (preLen + 2) >= 0) {
-              // 如果是 ....((..))
+              /* 如果是 ....((..)) */
               dp[i] = preLen + 2 + dp[i - (dp[i - 1] + 2)];
             } else {
-              // 否则是 ((..))
+              /* 否则是 ((..)) */
               dp[i] = preLen + 2;
             }
           } else {
@@ -54,7 +70,37 @@ public class T32_longest_valid_parentheses {
         }
         maxAnswer = Math.max(maxAnswer, dp[i]);
       }
+      printDP(s, dp);
       return maxAnswer;
+    }
+  }
+
+  /**
+   * 官方的写法比我简洁好多。
+   * 可以通过函数 printDP(s,dp) 看出，对于 ")(())()))" 我和官方写法，dp数组值是完全一样的。
+   * 再仔细看看代码，感觉官方写法只是省略了 dp[i] = 0 的赋值。
+   * 因为Java的数组默认值是0的，所以可以省略，但是没必要。我的代码中还是保留吧。
+   **/
+  static class Solution_leecode {
+
+    public int longestValidParentheses(String s) {
+      int maxans = 0;
+      int[] dp = new int[s.length()];
+      for (int i = 1; i < s.length(); i++) {
+        if (s.charAt(i) == ')') {
+          if (s.charAt(i - 1) == '(') {
+            dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+          } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+            dp[i] =
+              dp[i - 1] +
+              ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) +
+              2;
+          }
+          maxans = Math.max(maxans, dp[i]);
+        }
+      }
+      printDP(s, dp);
+      return maxans;
     }
   }
 }
