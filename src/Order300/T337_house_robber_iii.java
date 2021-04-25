63ad1ba28e22a69e44a590d19a870c19ca37151a
章一砚 <1052886775@qq.com>
2021-04-25 10:57:18 +0800

    T337 终极算法 0ms => 100%

--- src/Order300/T337_house_robber_iii.java
+++ src/Order300/T337_house_robber_iii.java
@@ -13,9 +13,11 @@ public class T337_house_robber_iii {
       .setLeft(TreeNode.from(1).setLeft(TreeNode.from(2)))
       .setRight(TreeNode.from(0).setLeft(TreeNode.from(3)));
     // -> 9
-    System.out.println(new Solution_me_暴力递归().rob(root));
-    System.out.println(new Solution_other().rob(root));
-    System.out.println(new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_梦中get终极算法().rob(root));
+    System.out.printf("%d ", new Solution_other().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构记忆化().rob(root));
+    System.out.printf("%d ", new Solution_me_终极算法().rob(root));
     System.out.println();
 
     root =
@@ -24,9 +26,11 @@ public class T337_house_robber_iii {
         .setLeft(TreeNode.from(2).setRight(TreeNode.from(3)))
         .setRight(TreeNode.from(3).setRight(TreeNode.from(1)));
     // -> 7
-    System.out.println(new Solution_me_暴力递归().rob(root));
-    System.out.println(new Solution_other().rob(root));
-    System.out.println(new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_梦中get终极算法().rob(root));
+    System.out.printf("%d ", new Solution_other().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构记忆化().rob(root));
+    System.out.printf("%d ", new Solution_me_终极算法().rob(root));
     System.out.println();
 
     root =
@@ -37,13 +41,23 @@ public class T337_house_robber_iii {
         )
         .setRight(TreeNode.from(5).setRight(TreeNode.from(1)));
     // -> 9
-    System.out.println(new Solution_me_暴力递归().rob(root));
-    System.out.println(new Solution_other().rob(root));
-    System.out.println(new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_梦中get终极算法().rob(root));
+    System.out.printf("%d ", new Solution_other().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构递归().rob(root));
+    System.out.printf("%d ", new Solution_me_最优子结构记忆化().rob(root));
+    System.out.printf("%d ", new Solution_me_终极算法().rob(root));
     System.out.println();
   }
 
-  static class Solution_me_暴力递归 {
+  /** 算法思想是好的，但是代码设计不够优秀。
+   * BUG 在于
+   * 如果 root 不偷，则儿子可偷可不偷！
+   * 并不是说 root 不偷，儿子就一定得 偷 才能是最大的！
+   *
+   * BUG就不修复了，放着吧。
+   * 修复了的见 Solution_me_终极算法
+   */
+  static class Solution_me_梦中get终极算法 {
 
     public int rob(TreeNode root) {
       /* 鲁棒性 特殊条件 输入过少 */
@@ -70,27 +84,6 @@ public class T337_house_robber_iii {
           (root.right == null ? 0 : robSubTree(root.right, !isRootSelected))
         );
       }
-    }
-  }
-
-  static class Solution_other {
-
-    public int rob(TreeNode root) {
-      int[] result = robInternal(root);
-      return Math.max(result[0], result[1]);
-    }
-
-    public int[] robInternal(TreeNode root) {
-      if (root == null) return new int[2];
-      int[] result = new int[2];
-
-      int[] left = robInternal(root.left);
-      int[] right = robInternal(root.right);
-
-      result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-      result[1] = left[0] + right[0] + root.val;
-
-      return result;
     }
   }
 
@@ -138,10 +131,11 @@ public class T337_house_robber_iii {
     }
   }
 
-  /** 
+  /**
    * 问题结构是数，所以dp不方便用数组，就用 HashMap 存储 node:TreeNode 和 rob(node) 的对应关系。
-   * 效率：3ms => 54.44% */
-  static class Solution {
+   * 效率：3ms => 54.44%
+   **/
+  static class Solution_me_最优子结构记忆化 {
 
     /** 安全的取值函数 —— 无惧输入为 null */
     private int val(TreeNode node) {
@@ -183,6 +177,64 @@ public class T337_house_robber_iii {
       int robRoot = Math.max(get_5, get_2);
       dpTable.put(root, robRoot);
       return robRoot;
+    }
+  }
+
+  /* 淦 终极解法 感觉和我一开始的想法完全一样啊，只是他代码设计的比我好 呜呜呜 */
+  /**
+   * 分两种情况：
+   * - 偷自己 ：val(root) + rob(left,false) + rob(right,false)
+   * - 不偷自己 ：rob(left,true) + rob(right,true)
+   *
+   * 这样的递归不存在 重复子问题 ,也就不需要 HashMap 记忆化来优化了
+   * 效率 0ms => 100%
+   */
+  static class Solution_me_终极算法 {
+
+    public int rob(TreeNode root) {
+      int[] result = rubAll(root);
+      return Math.max(result[0], result[1]);
+    }
+
+    /**
+     * out[0] : 偷root
+     * out[1] : 不偷root
+     */
+    private int[] rubAll(TreeNode root) {
+      if (root == null) {
+        return new int[2];
+      }
+      /* root 肯定不是 null 也就不需要安全性输入了 */
+      int[] left = rubAll(root.left);
+      int[] right = rubAll(root.right);
+      return new int[] {
+        /* 偷了 root 则儿子只能不偷 */
+        root.val + left[1] + right[1],
+        /* 没偷 root 则儿子可偷可不偷，要大的！ */
+        Math.max(left[0], left[1]) + Math.max(right[0], right[1]),
+      };
+    }
+  }
+
+  /** 大佬的终极算法代码实现 */
+  static class Solution_other {
+
+    public int rob(TreeNode root) {
+      int[] result = robInternal(root);
+      return Math.max(result[0], result[1]);
+    }
+
+    public int[] robInternal(TreeNode root) {
+      if (root == null) return new int[2];
+      int[] result = new int[2];
+
+      int[] left = robInternal(root.left);
+      int[] right = robInternal(root.right);
+
+      result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+      result[1] = left[0] + right[0] + root.val;
+
+      return result;
     }
   }
 }
