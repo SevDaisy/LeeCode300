3c129f95b2d5b575aabd815634de754fedf857c8
章一砚 <1052886775@qq.com>
2021-04-25 08:24:02 +0800

    T337 自己想的递归求解 不知道为什么有错

--- src/Order300/T337_house_robber_iii.java
+++ src/Order300/T337_house_robber_iii.java
@@ -7,7 +7,30 @@ public class T337_house_robber_iii {
   static class Solution {
 
     public int rob(TreeNode root) {
-      return 0;
+      /* 鲁棒性 特殊条件 输入过少 */
+      if (root == null) {
+        return 0;
+      }
+      return Math.max(robSubTree(root, true), robSubTree(root, false));
+    }
+
+    private int robSubTree(TreeNode root, boolean isRootSelected) {
+      /* 仔细看 我的程序 保证了 robSubTree 的 root 不是 null */
+      if (root == null) throw new RuntimeException(
+        "robSubTree's root shouldn't be NULL !!!"
+      );
+      if (isRootSelected) {
+        return (
+          root.val +
+          (root.left == null ? 0 : robSubTree(root.left, !isRootSelected)) +
+          (root.right == null ? 0 : robSubTree(root.right, !isRootSelected))
+        );
+      } else {
+        return (
+          (root.left == null ? 0 : robSubTree(root.left, !isRootSelected)) +
+          (root.right == null ? 0 : robSubTree(root.right, !isRootSelected))
+        );
+      }
     }
   }
 }
