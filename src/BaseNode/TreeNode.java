a7c5cbc93540d6700fe45ea5e55bbbc9fc291263
章一砚 <1052886775@qq.com>
2021-04-25 09:38:51 +0800

    TreeNode 节点增加 static from(int) 和 Builder的setter

--- src/BaseNode/TreeNode.java
+++ src/BaseNode/TreeNode.java
@@ -12,9 +12,28 @@ public class TreeNode {
     this.val = val;
   }
 
+  public static TreeNode from(int val) {
+    return new TreeNode(val);
+  }
+
   public TreeNode(int val, TreeNode left, TreeNode right) {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+
+  public TreeNode setVal(int val) {
+    this.val = val;
+    return this;
+  }
+
+  public TreeNode setLeft(TreeNode left) {
+    this.left = left;
+    return this;
+  }
+
+  public TreeNode setRight(TreeNode right) {
+    this.right = right;
+    return this;
   }
 }
