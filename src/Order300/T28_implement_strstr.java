5dfc39c77944b2696c0eb7c4ac7f5f8958c9cccc
章一砚 <1052886775@qq.com>
2021-04-28 15:39:55 +0800

    T28 跳过，等下次娘子陪我一起再学

---
+++ src/Order300/T28_implement_strstr.java
@@ -0,0 +1,14 @@
+package Order300;
+
+public class T28_implement_strstr {
+
+
+  /** KMP!!! */
+  static class Solution {
+
+    public int strStr(String haystack, String needle) {
+      
+      return 0;
+    }
+  }
+}
