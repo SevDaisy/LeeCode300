bf1e5f135563d2e26900d90db00afd8868fb0709
章一砚 <1052886775@qq.com>
2021-04-28 15:43:11 +0800

    T30 准备开始做

---
+++ src/Order300/T30_substring_with_concatenation_of_all_words.java
@@ -0,0 +1,13 @@
+package Order300;
+
+import java.util.List;
+
+public class T30_substring_with_concatenation_of_all_words {
+
+  static class Solution {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+      return null;
+    }
+  }
+}
