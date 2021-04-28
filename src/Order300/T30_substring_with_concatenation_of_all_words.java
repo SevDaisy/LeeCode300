c160f74536d21bbbcdda59eb14c098f15e3b1746
章一砚 <1052886775@qq.com>
2021-04-28 20:18:44 +0800

    T30 多起点滑动窗口法 17ms => 80.94%

--- src/Order300/T30_substring_with_concatenation_of_all_words.java
+++ src/Order300/T30_substring_with_concatenation_of_all_words.java
@@ -9,22 +9,37 @@ import java.util.Set;
 public class T30_substring_with_concatenation_of_all_words {
 
   public static void main(String[] args) {
-    System.out.println(
-      new Solution()
-      .findSubstring("barfoothefoobarman", new String[] { "foo", "bar" })
-    );
-    System.out.println(
-      new Solution()
-      .findSubstring(
-          "barfoofoobarthefoobarman",
-          new String[] { "bar", "foo", "the" }
-        )
-    );
+    String s;
+    String words[];
+
+    s = "barfoothefoobarman";
+    words = new String[] { "foo", "bar" };
+    System.out.println(new Solution().findSubstring(s, words));
+
+    s = "barfoofoobarthefoobarman";
+    words = new String[] { "bar", "foo", "the" };
+    System.out.println(new Solution().findSubstring(s, words));
+
+    s = "wordgoodgoodgoodbestword";
+    words = new String[] { "word", "good", "best", "good" };
+    System.out.println(new Solution().findSubstring(s, words));
+
+    StringBuilder sb = new StringBuilder("a");
+    words = new String[5000];
+    for (int i = 0; i < 5000; i++) {
+      words[i] = sb.toString();
+    }
+    for (int i = 1; i < 5000; i++) {
+      sb.append('a');
+    }
+    s = sb.toString();
+    System.out.println(new Solution().findSubstring(s, words));
   }
 
-  static class Solution {
+  public static class Solution {
 
     public List<Integer> findSubstring(String s, String[] words) {
+      List<Integer> answers = new ArrayList<>();
       if (
         s == null ||
         s.length() == 0 ||
@@ -32,49 +47,57 @@ public class T30_substring_with_concatenation_of_all_words {
         words.length == 0 ||
         s.length() < words[0].length()
       ) {
-        return null;
+        return answers;
       }
-      List<Integer> answers = new ArrayList<>();
       int iMax = s.length();
       int wordsSize = words.length;
       int step = words[0].length();
       int L = step * wordsSize;
-      Map<String, Integer> map = new HashMap<String, Integer>(wordsSize) {
+      Map<String, Integer> requireMap = new HashMap<String, Integer>(
+        wordsSize
+      ) {
         {
           for (int i = 0; i < wordsSize; i++) {
-            put(words[i], 0);
+            if (keySet().contains(words[i])) {
+              put(words[i], get(words[i]) + 1);
+            } else put(words[i], 1);
           }
         }
       };
-      Set<String> keySet = map.keySet();
+      Map<String, Integer> windowMap = new HashMap<String, Integer>(wordsSize);
+      Set<String> keySet = requireMap.keySet();
 
       for (int begin = 0; begin < step; begin++) {
-        int matchedCnt = 0;
-        for (int left = begin; left + L < iMax; left += step) {
+        for (String key : keySet) {
+          windowMap.put(key, 0);
+        }
+        for (int left = begin; left + L <= iMax; left += step) {
           if (left == begin) {
             for (int i = left; i + step <= left + L; i += step) {
               String cur = s.substring(i, i + step);
               if (keySet.contains(cur)) {
-                int curCnt = map.get(cur);
-                if (curCnt == 0) matchedCnt++;
-                map.put(cur, curCnt + 1);
+                windowMap.put(cur, windowMap.get(cur) + 1);
               }
             }
           } else {
             String _old = s.substring(left - step, left);
             String _new = s.substring(left + L - step, left + L);
             if (keySet.contains(_old)) {
-              int cnt = map.get(_old);
-              if (cnt == 1) matchedCnt--;
-              map.put(_old, cnt - 1);
+              windowMap.put(_old, windowMap.get(_old) - 1);
             }
             if (keySet.contains(_new)) {
-              int cnt = map.get(_new);
-              if (cnt == 0) matchedCnt++;
-              map.put(_new, cnt + 1);
+              windowMap.put(_new, windowMap.get(_new) + 1);
             }
           }
-          if (matchedCnt == wordsSize) {
+          boolean isOK = true;
+          for (String key : keySet) {
+            /* Integer 包装类 的判断相等，不能用 == 那是比较内存地址的 */
+            if (!requireMap.get(key).equals(windowMap.get(key))) {
+              isOK = false;
+              break;
+            }
+          }
+          if (isOK) {
             answers.add(left);
           }
         }
