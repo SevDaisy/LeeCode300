39c2aaa1df7d8f8ae53f678e4597227174d64ba6
章一砚 <1052886775@qq.com>
2021-07-12 19:24:47 +0800

    T38 改成递归写法。居然这么快。1ms => 98.11%

--- src/Order300/T38_count_and_say.java
+++ src/Order300/T38_count_and_say.java
@@ -11,9 +11,38 @@ public class T38_count_and_say {
     System.out.println(new Solution().countAndSay(5));
   }
 
+  /* 第二次尝试 - 递归 时间 1ms => 98.11% 空间 36.4MB => 33.98% */
   static class Solution {
 
-    /* 第一次尝试 时间 5ms => 48.80% 空间 37.9MB => 25.42% */
+    // 递归写法
+    public String countAndSay(int n) {
+      if (n == 1) {
+        return "1";
+      }
+      StringBuilder builder = new StringBuilder();
+      String history = countAndSay(n - 1);
+      int cnt = 0;
+      char k = history.charAt(0);
+      for (char ch : history.toCharArray()) {
+        if (k == ch) {
+          cnt++;
+        } else {
+          builder.append(cnt);
+          builder.append(k);
+          cnt = 1;
+          k = ch;
+        }
+      }
+      builder.append(cnt);
+      builder.append(k);
+
+      return builder.toString();
+    }
+  }
+
+  /* 第一次尝试 时间 5ms => 48.80% 空间 37.9MB => 25.42% */
+  static class Solution_fi {
+
     public String countAndSay(int n) {
       int loops = 1;
       String str = "1";
