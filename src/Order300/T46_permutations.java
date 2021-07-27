1dee259cee03282854c9c67ecee684a49848e94e
章一砚 <1052886775@qq.com>
2021-07-27 15:42:38 +0800

    T46 自己的写法 第一次尝试 时间 1ms => 93.95%

--- src/Order300/T46_permutations.java
+++ src/Order300/T46_permutations.java
@@ -1,47 +1,34 @@
 package Order300;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class T46_permutations {
 
+  /* 第一次尝试 时间 1ms => 93.95% 空间 38.4MB => 90.16% */
   static class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
       List<List<Integer>> out = new ArrayList<>();
-      backtrack(
-        nums,
-        0,
-        out,
-        new ArrayList<Integer>() {
-          {
-            for (int x : nums) {
-              add(x);
-            }
-          }
-        }
-      );
+      backtrack(nums, 0, out, new ArrayList<Integer>());
       return out;
     }
 
     private void backtrack(
       int[] nums,
-      int pos,
+      int step,
       List<List<Integer>> ans,
       List<Integer> path
     ) {
-      if (pos == nums.length) {
+      if (step == nums.length) {
         ans.add(new ArrayList<>(path));
         return;
       }
-      for (int i = pos; i < nums.length; i++) {
-        // 动态维护数组
-        Collections.swap(path, pos, i);
-        // 继续递归填下一个数
-        backtrack(nums, pos + 1, ans, path);
-        // 撤销操作
-        Collections.swap(path, pos, i);
+      for (int i = 0; i < nums.length; i++) {
+        if (path.contains(nums[i])) continue;
+        path.add(nums[i]);
+        backtrack(nums, step + 1, ans, path);
+        path.remove(path.size() - 1);
       }
     }
   }
